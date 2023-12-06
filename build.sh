1027f94ecd6a7f691bdc7eeeb3c28933b460780b
Fritz 179 <fabrizio.cortesi179@gmail.com>
2023-12-06 16:20:32 +0100

    Added rezepte.c

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ CC="${CXX:-cc}"
 PKGS=""
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
 LIBS=-lm
-SRC="main_helfer_photonics_mobile_robotics.c geom.c mechanik.c elektronikAufgaben.c elektronikFormeln.c vector.c brechungsgesetz.c brechungsindex.c formel3.c"
+SRC="main_helfer_photonics_mobile_robotics.c geom.c rezepte.c mechanik.c elektronikAufgaben.c elektronikFormeln.c vector.c brechungsgesetz.c brechungsindex.c formel3.c"
 # $CC $CFLAGS `pkg-config --cflags $PKGS` -o main $SRC $LIBS `pkg-config --libs $PKGS`
 $CC $CFLAGS -o main $SRC $LIBS
